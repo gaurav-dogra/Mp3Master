2ee765589dcc3d8e2d9e96f3d5f0b4adeed945bf
Gaurav Dogra <gauru.dogra@gmail.com>
2021-02-16 23:53:29 +0000

    code refactoring with >> files == null { return; }

--- src/main/java/FolderScanner.java
+++ src/main/java/FolderScanner.java
@@ -12,16 +12,16 @@ public class FolderScanner {
     private static void scanMp3(File folder, List<File> mp3Files) {
         File[] files = folder.listFiles();
 
-        if (files != null) {
-            for (File currentFile : files) {
-                if (currentFile.isDirectory()) {
-                    scanMp3(currentFile, mp3Files);
-                } else {
-                    String name = currentFile.getName();
-                    String extension = name.substring(name.lastIndexOf(".") + 1);
-                    if (extension.equalsIgnoreCase("mp3")) {
-                        mp3Files.add(currentFile);
-                    }
+        if (files == null) { return; }
+
+        for (File currentFile : files) {
+            if (currentFile.isDirectory()) {
+                scanMp3(currentFile, mp3Files);
+            } else {
+                String name = currentFile.getName();
+                String extension = name.substring(name.lastIndexOf(".") + 1);
+                if (extension.equalsIgnoreCase("mp3")) {
+                    mp3Files.add(currentFile);
                 }
             }
         }
