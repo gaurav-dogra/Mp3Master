130dc2583694fad49e258e1a65937787c366c48e
Gaurav Dogra <gauru.dogra@gmail.com>
2021-02-16 21:08:07 +0000

    Changed Application.java to add Use newly created FolderScanner class, also added tests to FolderScanner

---
+++ src/main/java/FolderScanner.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FolderScanner {
+    public static List<File> scanMp3(String folderName) {
+        List<File> mp3Files = new ArrayList<>();
+        scanMp3(new File(folderName), mp3Files);
+        return mp3Files;
+    }
+
+    private static void scanMp3(File folder, List<File> mp3Files) {
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File currentFile : files) {
+                if (currentFile.isDirectory()) {
+                    scanMp3(currentFile, mp3Files);
+                } else {
+                    String name = currentFile.getName();
+                    String extension = name.substring(name.lastIndexOf(".") + 1);
+                    if (extension.equalsIgnoreCase("mp3")) {
+                        mp3Files.add(currentFile);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/FolderScannerTest.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FolderScannerTest {
+
+    private static List<File> mp3Files;
+    private static final String[] tempTestData = {"sample", "sample/my1.mp3", "sample/my2.mp3",
+            "sample/my3.mp3", "sample/my4.mp3", "sample/my5.mp3",
+            "sample/myF1", "sample/myF2", "sample/myF2/my6.mp3",
+            "sample/myF1/noMP3.jpg", "sample/myF2/my7.mp3", "sample/myF1/my8.mp3"};
+    private static final List<File> expectedFiles = new ArrayList<>();
+
+    @BeforeAll
+    static void init() {
+        Arrays.stream(tempTestData).map(Path::of).forEach((s) -> {
+            if (s.toString().contains(".")) {
+                try {
+                    s.toFile().createNewFile();
+                    if (s.toString().endsWith(".mp3")) {
+                        expectedFiles.add(s.toFile());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                s.toFile().mkdir();
+            }
+        });
+
+        mp3Files = FolderScanner.scanMp3(tempTestData[0]);
+    }
+
+    @AfterAll
+    static void cleanUp() {
+        new File("sample/myF1/my8.mp3").delete();
+        new File("sample/myF1/noMP3.jpg").delete();
+        new File("sample/myF1").delete();
+        new File("sample/myF2/my6.mp3").delete();
+        new File("sample/myF2/my7.mp3").delete();
+        new File("sample/myF2").delete();
+        new File("sample/my1.mp3").delete();
+        new File("sample/my2.mp3").delete();
+        new File("sample/my3.mp3").delete();
+        new File("sample/my4.mp3").delete();
+        new File("sample/my5.mp3").delete();
+        new File("sample").delete();
+    }
+
+    @Test
+    void start() {
+        assertEquals(8, mp3Files.size(), "Expected: 8, Actual: " + mp3Files.size());
+        assertTrue(expectedFiles.containsAll(mp3Files), "Files are different");
+        assertEquals(0, FolderScanner.scanMp3("").size(), "empty String gets empty list");
+    }
+}
