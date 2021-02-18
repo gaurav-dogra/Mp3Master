c7eece7d441de2016c9f231ceeb93397a81799c9
Gaurav Dogra <gauru.dogra@gmail.com>
2021-02-18 01:13:19 +0000

    wrote custom equals &  hash code. ignoring id field

--- src/main/java/model/Song.java
+++ src/main/java/model/Song.java
@@ -6,7 +6,6 @@ import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
-@EqualsAndHashCode
 @ToString
 public class Song {
     private final int id;
@@ -14,4 +13,26 @@ public class Song {
     private final String artist;
     private final String album;
     private final String year;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Song song = (Song) o;
+
+        if (getTitle() != null ? !getTitle().equals(song.getTitle()) : song.getTitle() != null) return false;
+        if (getArtist() != null ? !getArtist().equals(song.getArtist()) : song.getArtist() != null) return false;
+        if (getAlbum() != null ? !getAlbum().equals(song.getAlbum()) : song.getAlbum() != null) return false;
+        return getYear() != null ? getYear().equals(song.getYear()) : song.getYear() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getTitle() != null ? getTitle().hashCode() : 0;
+        result = 31 * result + (getArtist() != null ? getArtist().hashCode() : 0);
+        result = 31 * result + (getAlbum() != null ? getAlbum().hashCode() : 0);
+        result = 31 * result + (getYear() != null ? getYear().hashCode() : 0);
+        return result;
+    }
 }
