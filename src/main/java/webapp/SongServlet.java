407a1c176453dbafd6e2aa9c8a62be18c0c0c89d
Viktor Martynov <2013alfabravo@gmail.com>
2021-02-22 18:45:53 +0400

    fixed connection provider, added basic song mapping to html table

--- src/main/java/webapp/SongServlet.java
+++ src/main/java/webapp/SongServlet.java
@@ -1,10 +1,14 @@
 package webapp;
 
+import model.Song;
+import model.SongDaoImpl;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -14,12 +18,34 @@ public class SongServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         String message = new Scanner(Objects.requireNonNull(HomeServlet.class.getClassLoader()
-                .getResourceAsStream("song_table.html")),
-                StandardCharsets.UTF_8)
+                .getResourceAsStream("song_table.html")), StandardCharsets.UTF_8)
                 .useDelimiter("\\Z")
                 .next();
 
-        response.getWriter().println(message);
+        SongDaoImpl songDao = new SongDaoImpl();
+        songDao.connect("Mp3Master.db");
+        List<Song> songs = songDao.getAll();
+
+        StringBuilder builder = new StringBuilder();
+
+        for (Song song : songs) {
+            int id = song.getId();
+            String title = song.getTitle();
+            String artist = song.getArtist();
+            String album = song.getAlbum();
+            String year = song.getYear();
+            builder.append("<tr>")
+                    .append("<td>")
+                    .append(id).append("</td><td>")
+                    .append(title).append("</td><td>")
+                    .append(artist).append("</td><td>")
+                    .append(album).append("</td><td>")
+                    .append(year).append("</td>")
+                    .append("</tr>");
+        }
+
+        String[] segments = message.split("<!--Data rows-->");
+        response.getWriter().println(segments[0] + builder.toString() + segments[1]);
 
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
