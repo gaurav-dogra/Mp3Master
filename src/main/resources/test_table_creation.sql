25ee73e4b8a38f57cd233c1e071be116b73c32dd
Viktor Martynov <2013alfabravo@gmail.com>
2021-02-23 21:34:53 +0400

    fixed strings in the sql file

--- src/main/resources/test_table_creation.sql
+++ src/main/resources/test_table_creation.sql
@@ -5,44 +5,44 @@ CREATE TABLE test_songs IF NOT EXISTS (
 	album TEXT, 
 	year TEXT);
 
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 1, Artist 1, Album 1, 2000);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 2, Artist 1, Album 1, 2000);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 3, Artist 1, Album 1, 2000);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 4, Artist 1, Album 1, 2000);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 5, Artist 1, Album 1, 2000);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 6, Artist 1, Album 1, 2000);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 1", "Artist 1", "Album 1", "2000");
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 2", "Artist 1", "Album 1", "2000);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 3", "Artist 1", "Album 1", "2000);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 4", "Artist 1", "Album 1", "2000);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 5", "Artist 1", "Album 1", "2000);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 6", "Artist 1", "Album 1", "2000);
 
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 7, Artist 2, Album 2, 2010);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 8, Artist 2, Album 2, 2010);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 9, Artist 2, Album 2, 2010);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 10, Artist 2, Album 2, 2010);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 11, Artist 2, Album 2, 2010);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 12, Artist 2, Album 2, 2010);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 7", "Artist 2", "Album 2", "2010);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 8", "Artist 2", "Album 2", "2010);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 9", "Artist 2", "Album 2", "2010);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 10", "Artist 2", "Album 2", "2010);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 11", "Artist 2", "Album 2", "2010);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 12", "Artist 2", "Album 2", "2010);
 
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 13, Artist 3, Album 3, 2015);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 14, Artist 1, Album 3, 2002);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 15, Artist 1, Album 3, 2009);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 16, Artist 2, Album 3, 2020);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 17, Artist 2, Album 3, 2018);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 18, Artist 3, Album 3, 2011);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 13", "Artist 3", "Album 3", "2015);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 14", "Artist 1", "Album 3", "2002);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 15", "Artist 1", "Album 3", "2009);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 16", "Artist 2", "Album 3", "2020);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 17", "Artist 2", "Album 3", "2018);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 18", "Artist 3", "Album 3", "2011);
 
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 19, Artist 3, Album 4, 2013);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 20, Artist 3, Album 4, 2013);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 21, Artist 3, Album 4, 2013);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 22, Artist 3, Album 4, 2013);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 23, Artist 3, Album 4, 2013);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 24, Artist 3, Album 4, 2013);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 19", "Artist 3", "Album 4", "2013);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 20", "Artist 3", "Album 4", "2013);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 21", "Artist 3", "Album 4", "2013);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 22", "Artist 3", "Album 4", "2013);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 23", "Artist 3", "Album 4", "2013);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 24", "Artist 3", "Album 4", "2013);
 
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 25, Artist 1, Album 5, 2016);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 26, Artist 1, Album 5, 2016);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 27, Artist 1, Album 5, 2016);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 28, Artist 1, Album 5, 2016);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 29, Artist 1, Album 5, 2016);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 30, Artist 1, Album 5, 2016);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 25", "Artist 1", "Album 5", "2016);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 26", "Artist 1", "Album 5", "2016);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 27", "Artist 1", "Album 5", "2016);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 28", "Artist 1", "Album 5", "2016);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 29", "Artist 1", "Album 5", "2016);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 30", "Artist 1", "Album 5", "2016);
 
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 31, Artist 3, Album 6, 2017);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 32, Artist 3, Album 6, 2017);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 33, Artist 3, Album 6, 2017);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 34, Artist 3, Album 6, 2017);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 35, Artist 3, Album 6, 2017);
-INSERT INTO test_songs (title, artist, album, year) VALUES(Title 36, Artist 3, Album 6, 2017);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 31", "Artist 3", "Album 6", "2017);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 32", "Artist 3", "Album 6", "2017);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 33", "Artist 3", "Album 6", "2017);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 34", "Artist 3", "Album 6", "2017);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 35", "Artist 3", "Album 6", "2017);
+INSERT INTO test_songs (title, artist, album, year) VALUES("Title 36", "Artist 3", "Album 6", "2017);
